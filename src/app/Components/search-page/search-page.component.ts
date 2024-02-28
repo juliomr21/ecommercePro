cf53c8fc2a150dc5f50a86096740857dbb1b4db6
juliomr21 <juliomr21.correo.com>
2024-02-28 00:35:25 -0300

    prod

--- src/app/Components/search-page/search-page.component.ts
+++ src/app/Components/search-page/search-page.component.ts
@@ -1,5 +1,5 @@
 
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpConectionService } from '../../service/http-conection.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -12,27 +12,32 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class SearchPageComponent {
   url = 'https://dummyjson.com/products/search?q=';
-  show_list_products:any[] = [];
+  show_list_products: any[] = [];
   notfound = true;
-constructor(private http:HttpConectionService,private queryRoute:ActivatedRoute, private router: Router){
+  carga = false;
+  constructor(private http: HttpConectionService, private queryRoute: ActivatedRoute, private router: Router) {
 
-}
+  }
 
-ngOnInit(){
-  
-   this.queryRoute.queryParams.subscribe(params => {
-    let temp:any = params;
-    this.http.get(this.url + temp.q).subscribe(resp => {
-      let list_aux:any = resp;
-      this.show_list_products = list_aux.products;
-      if(this.show_list_products.length != 0)
-      this.notfound = false;
-      
+  ngOnInit() {
+
+    this.queryRoute.queryParams.subscribe(params => {
+      this.carga = false;
+      this.notfound = true;
+      let temp: any = params;
+      this.http.get(this.url + temp.q).subscribe(resp => {
+        this.carga = true;
+        let list_aux: any = resp;
+        this.show_list_products = list_aux.products;
+
+        if (this.show_list_products.length != 0)
+          this.notfound = false;
+
+      })
     })
-})
-}
-go_to_product_detail(id:number){
-  this.router.navigateByUrl(`/product-detail/${id}`);
-}
+  }
+  go_to_product_detail(id: number) {
+    this.router.navigateByUrl(`/product-detail/${id}`);
+  }
 
 }
