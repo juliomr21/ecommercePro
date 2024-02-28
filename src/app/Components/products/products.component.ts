633f96fd4daf7e8f654b291d719bad035e05f6c0
juliomr21 <juliomr21.correo.com>
2024-02-28 19:04:23 -0300

    filter para ver

--- src/app/Components/products/products.component.ts
+++ src/app/Components/products/products.component.ts
@@ -20,6 +20,7 @@ export class ProductsComponent {
   brand_list:brandType[] = [];
   categoryMark:string[] = [];
   brandMark:brandType[] = [];
+  orderType = 1;
   categoryHeight = {  "height": "195px", "overflow-y": "hidden", "overflow-x": "hidden"}
   brandHeight = {
     "height": "195px", "overflow-y": "hidden", "overflow-x": "hidden"
@@ -27,7 +28,7 @@ export class ProductsComponent {
   }
   price_min = 0;
   price_max = 10000;
-  
+  filterVar = false
   show_brand = true;
   show_category = true;
   constructor(private http: HttpConectionService,
@@ -54,7 +55,9 @@ export class ProductsComponent {
   
   init_data() {
     this.activate_route.params.subscribe(resp => {
-      next: {
+       {
+        console.log('ssss');
+        this.filterVar = false;
         let temp: any = resp;
         this.category = temp.category;
         if (this.category != 'all') {
@@ -70,7 +73,7 @@ export class ProductsComponent {
           let templist_products: any = resp;
           this.list_products = templist_products.products;
           this.show_list_products = this.list_products;
-          
+
         })
       }
     });
@@ -80,10 +83,10 @@ export class ProductsComponent {
      
       for (let item of this.full_list) {
         if (item.id % 5 == 0)
-          this.category_list.push(item.category);
-       
+          this.category_list.push(item.category); 
+          
       }
-     
+     this.add_categ(this.category)
     })
   }
   change_ruta() {
@@ -158,21 +161,48 @@ export class ProductsComponent {
     console.log(this.brandMark)
   }
   filter_show(){
-   
+   this.filterVar = false;
     if(this.categoryMark.length == 0 && this.brandMark.length == 0)
     {
       
       this.show_list_products = this.full_list.filter(item => item.price >= this.price_min && item.price <= this.price_max);
+      this.my_sort();
       return;
     }
    
     if(this.brandMark.length == 0 && this.categoryMark.length != 0){
      
       this.show_list_products = this.full_list.filter(item => this.categoryMark.find(it => it == item.category) && (item.price >= this.price_min && item.price <= this.price_max))
+      this.my_sort();
       return;
     }
     this.show_list_products = this.full_list.filter(item => 
       this.brandMark.find(it => item.brand == it.brand && item.category == it.category) && item.price >= this.price_min && item.price <= this.price_max);
+      this.my_sort();
+    }
+    my_sort(){
+      if(this.orderType == 1)
+       this.sort_feat();
+      if(this.orderType == 2)
+      this.sort_high_to_low()
+    if(this.orderType == 3)
+    this.sort_low_to_high()
+    }
+  sort_high_to_low(){
+    this.show_list_products.sort((a,b)=>{
+     return  a.price - b.price;
+    });
+    this.orderType = 2;
+  }
+  sort_low_to_high(){
+    this.show_list_products.sort((a,b)=>{
+     return  b.price -  a.price ;
+    })
+    this.orderType = 3;
+  }
+  sort_feat(){
+    this.show_list_products.sort((a,b) => a.rating - b.rating);
+    this.orderType = 1;
   }
 }
 export interface brandType{
